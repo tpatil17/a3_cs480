59a5b5f9a2d00884fa33492df4e6a820a77e8af1
Tanishq Patil <59779592+tpatil17@users.noreply.github.com>
2024-10-27 14:51:38 -0700

    Optional arguments

---
+++ Makefile
@@ -0,0 +1,47 @@
+#*************************************************************
+# Name: Tanishq Patil
+# Red Id: 132639686
+# Assignment 3
+#*************************************************************
+
+#Makefile
+
+#.RECIPEPREFIX +=
+
+# Specify compiler
+CC = gcc
+# Compiler flags, if you want debug info, add -g
+CCFLAGS = -std=c11 -g -Wall -c
+CFLAGS = -g -c
+
+# object files
+OBJS = main.o #PageTableLevel.o BitMasker.o tracereader.o main.o log.o
+
+# Program name
+PROGRAM = pagetrace
+
+# The program depends upon its object files
+$(PROGRAM) : $(OBJS)
+	$(CC) -o $(PROGRAM) $(OBJS)
+
+main.o : main.c 
+	$(CC) $(CCFLAGS) main.c
+
+#PageTableLevel.o : PageTableLevel.c PageTableLevel.h 
+#	$(CC) $(CCFLAGS) PageTableLevel.c
+
+#BitMasker.o : BitMasker.c BitMasker.h
+#	$(CC) $(CCFLAGS) BitMasker.c
+
+#tracereader.o : tracereader.c tracereader.h
+#	$(CC) $(CCFLAGS) tracereader.c
+
+#log.o : log.c log.h
+#	$(CC) $(CCFLAGS) log.c
+
+# Once things work, people frequently delete their object files.
+# If you use "make clean", this will do it for you.
+# As we use gnuemacs which leaves auto save files termintating
+# with ~, we will delete those as well.
+clean :
+	rm -rf $(OBJS) *~ $(PROGRAM)
---
+++ main.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<getopt.h>
+#include<unistd.h>
+
+#define NORMAL_EXIT 0
+#define READ_BUFFER 1024
+
+int done_simulation = 0; // to busy wait the main thread till worker finshes
+
+
+int main(int argc, char* argv[]){
+
+    int option = 0;
+    unsigned int n = 0;
+    unsigned int tlb_sz = 0;
+    char* log_mode= "summary";
+    char* filename = NULL;
+
+    FILE* file;
+
+
+    
+    while ((option = getopt(argc, argv, "n:c:o")) != -1) { // identify if alpha is specified through command line
+        switch (option) {
+            case 'n':
+                n = strtof(optarg, NULL);  // number of addresses to process
+                if (n <= 0){
+                    printf("Number of memory accesses must be a number, greater than 0");
+                    exit(NORMAL_EXIT);
+                }else{
+                    //debug statement
+                    printf("the entered value for n: %u\n", n);
+                }
+                break;
+            case 'c': // the tlb cache size
+                tlb_sz = strtof(optarg, NULL);
+                if (tlb_sz <0){
+                    printf("Cache capacity must be a number, greater than or equal to 0");
+                    exit(NORMAL_EXIT);
+                }else{
+                    //debug statement
+                    printf("the entered value for c: %u\n", tlb_sz);
+                }
+                break;
+            case 'o': // the tlb cache size
+                log_mode = optarg;
+                //debug statement
+                printf("the entered value for o: %s\n", log_mode);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+// Get the file name
+
+    for (int i = optind; i < argc; i++) {
+        if (filename == NULL) {
+            filename = argv[i];  // Assuming first non optional arg is filename, according to online sources
+        } else {
+            printf("additional argument detected: %s\n", argv[i]);
+            exit(NORMAL_EXIT);
+        }
+    }
+
+// If file name is not found 
+    if (filename == NULL) {
+        printf("No file detected.\n");
+        exit(NORMAL_EXIT);
+    }
+
+// Read the contents from the file
+
+    file = fopen(filename, "r"); // open the file using the path
+
+    // Check for file validity and permissions
+    if(file == NULL){
+        printf("Unable to open <<%s>>", filename);
+        exit(NORMAL_EXIT);
+    }
+    
+
+//     char buffer[READ_BUFFER];
+//     char copy_buff[READ_BUFFER];
+//     char* token = NULL;
+//     unsigned int numProc = 0; // how many processes or lines do we have
+    
+// // count the number of lines
+
+//     while (fgets(buffer, READ_BUFFER, file) != NULL) {
+
+//         numProc += 1;
+//     }
+
+//     // Create a table to store the burst time for processes
+return 0;
+}
