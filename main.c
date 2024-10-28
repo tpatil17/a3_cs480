a26eb63010039ac2c8ed369160f157be6919107d
Tanishq Patil <59779592+tpatil17@users.noreply.github.com>
2024-10-28 15:43:12 -0700

    not processing big bits

--- main.c
+++ main.c
@@ -202,9 +202,7 @@ Root.page_size = (unsigned int)1 << (32-sum); // 2^offset bits is the page size
             ctr+=1;
         }
         if(strcmp(log_mode, "summary") == 0){
-
-            PageLevel* cursor = level0;
-            PageTable* table = &Root;
+            printf("in here\n");
             //table_entries(table, cursor);
             log_summary(Root.page_size, Root.cache_hit, Root.page_table_hit, ctr, Root.frame_count, Root.total_entry);
         }
